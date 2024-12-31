638e318e70225bae20b5420a0352b407fb61d449
Anup Khismatrao <khismatrao2003@gmail.com>
2024-12-31 19:03:07 +0530

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
-import "./App.css";
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Main from './pages/Main';
 
-function App() {
-  return <div>home</div>;
-}
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/main" element={<Main />} />
+      </Routes>
+    </Router>
+  );
+};
 
 export default App;
---
+++ src/pages/Main.jsx
@@ -0,0 +1,9 @@
+const Main = () => {
+  return (
+    <div>
+      <h1>This is the Main Page</h1>
+    </div>
+  );
+};
+
+export default Main;
